f72a7f553993fc86cf5f83beacada33cbdcbf562
Stas Isackov <1123889+sisackov@users.noreply.github.com>
2019-08-28 23:34:34 +0300

    demo button on map

--- app/src/main/java/com/buddiesmap/MainActivity.java
+++ app/src/main/java/com/buddiesmap/MainActivity.java
@@ -1,8 +1,8 @@
 package com.buddiesmap;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,17 +15,58 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+//    final Button homeButton = null;
+//    final Button locButton = null;
+//    final Button chkButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-        setContentView(R.layout.map_main);
+        setContentView(R.layout.activity_maps);
+//        setContentView(R.layout.map_main);
 
+//        final Button homeButton = (Button) findViewById(R.id.hometownButton);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        // Button to control hometown overlay
+//        final Button homeButton = (Button) findViewById(R.id.hometownButton);
+//        homButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v)
+//            {
+//                if(Globals.isRefreshed)
+//                    Main.this.runOnUiThread(new Runnable() {
+//                        public void run(){setHomeOverlay();}});
+//            }
+//        });
+
+        // Button to control location overlay
+//        final Button locButton = (Button) findViewById(R.id.locButton);
+//        locButton.setOnClickListener(new OnClickListener(){
+//            public void onClick(View v)
+//            {
+//                if(Globals.isRefreshed)
+//                    Main.this.runOnUiThread(new Runnable() {
+//                        public void run(){setLocationOverlay();}});
+//            }
+//        });
+
+        // Button to control checkin overlay
+//        final Button chkButton = (Button) findViewById(R.id.chckButton);
+//        chkButton.setOnClickListener(new OnClickListener(){
+//            public void onClick(View v)
+//            {
+//                if(!Globals.refreshInProgress)
+//                    Main.this.runOnUiThread(new Runnable() {
+//                        public void run(){setCheckinOverlay();}});
+//            }
+//        });
+
+//        Globals.stateButton = (Button)Globals.mapActivity.findViewById(R.id.stateButton);
     }
 
 
