5508e8f7127a1c83d6594ac5afd1514facc2a576
Stas Isackov <1123889+sisackov@users.noreply.github.com>
2019-08-28 13:23:24 +0300

    map works

--- app/src/main/java/com/buddiesmap/MainActivity.java
+++ app/src/main/java/com/buddiesmap/MainActivity.java
@@ -18,7 +18,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+//        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.map_main);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
